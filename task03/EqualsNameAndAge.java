6caf0100690084b5189a7e107b8cc0bcf68080e6
jirafa27 <jirafa27@mail.ru>
2019-04-19 08:43:44 +0300

    Дз

---
+++ task03/EqualsNameAndAge.java
@@ -0,0 +1,4 @@
+package part1.lesson01.task03;
+
+class EqualsNameAndAge extends Exception {
+}
---
+++ task03/Sort.java
@@ -0,0 +1,5 @@
+package part1.lesson01.task03;
+
+public interface Sort {
+    public  Person[] sort(Person[] arr);
+}
